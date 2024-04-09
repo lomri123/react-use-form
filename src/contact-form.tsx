4ce65f03d49f9a177b11e7a4971579f965bed874
omri <levomri@gmail.com>
2024-04-09 15:12:27 +0300

    added yup validation

--- src/contact-form.tsx
+++ src/contact-form.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import useForm from './use-form';
+import * as yup from 'yup';
 
 interface FormState {
   name: string;
@@ -7,23 +8,40 @@ interface FormState {
 }
 
 const handleSubmit = async (
-  data: FormState // formData is now a regular data
+  data: FormState,
+  errors: Record<keyof FormState, string>
 ) => {
   console.log('formData', data);
+  console.log('errors', errors);
 };
 
+const validationSchema = yup.object({
+  name: yup
+    .string()
+    .min(2, 'Too short!')
+    .max(50, 'Too long!')
+    .required('Required'),
+  email: yup.string().email('Invalid email').required('Required'),
+});
+
 const ContactForm: React.FC = () => {
-  const { formState, formAction } = useForm<FormState>(handleSubmit, {
-    name: '',
-    email: '',
-  });
+  const { formData, errors, register, formAction } = useForm<FormState>(
+    handleSubmit,
+    {
+      name: '',
+      email: '',
+    },
+    validationSchema
+  );
 
   return (
     <>
-      formState: {JSON.stringify(formState)}
+      formState: {JSON.stringify(formData)}
       <form action={formAction}>
-        <input name='name' />
-        <input name='email' />
+        <input {...register('name')} />
+        {errors.name && <div className='error-message'>{errors.name}</div>}
+        <input {...register('email')} />
+        {errors.email && <div className='error-message'>{errors.email}</div>}
         <input type='submit' />
       </form>
     </>
--- src/use-form.ts
+++ src/use-form.ts
@@ -1,33 +1,97 @@
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { useFormState, useFormStatus } from 'react-dom';
+import { ObjectSchema, ValidationError, reach } from 'yup';
 
 interface FormProps<T> {
   formAction: () => void;
   pending: boolean;
   formState: T;
+  formData: T;
   submitCount: number;
+  touched: Record<keyof T, boolean>;
+  errors: Record<keyof T, string>;
+  register: (name: keyof T) => {
+    name: keyof T;
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+    onBlur: (e: ChangeEvent<HTMLInputElement>) => void;
+  };
 }
 
-function useForm<T>(
-  handleSubmit: (data: T) => void,
-  initialState: T
+function useForm<T extends Record<string, any>>(
+  handleSubmit: (data: T, errors: Record<keyof T, string>) => void,
+  initialState: T,
+  validationSchema?: ObjectSchema<T>
 ): FormProps<T> {
   const [formState, formAction] = useFormState<T>(onSubmit, initialState);
   const { pending } = useFormStatus();
   const [submitCount, setSubmitCount] = useState(0);
+  const [touched, setTouched] = useState<Record<keyof T, boolean>>(
+    {} as Record<keyof T, boolean>
+  );
+  const [formData, setFormData] = useState(initialState);
+  const [errors, setErrors] = useState<Record<keyof T, string>>(
+    {} as Record<keyof T, string>
+  );
+
+  const validate = async (data: T) => {
+    let errors = {};
+    try {
+      await validationSchema?.validate(data, { abortEarly: false });
+    } catch (err) {
+      if (err instanceof ValidationError) {
+        const newErrors: Record<string, string> = {};
+        err.inner.forEach((error) => {
+          if (error.path) newErrors[error.path] = error.message;
+        });
+        errors = newErrors;
+      }
+    }
+    return errors as Record<keyof T, string>;
+  };
 
   async function onSubmit(previousState: T, formData: FormData): Promise<T> {
     setSubmitCount((count) => count + 1);
     const fieldValues = Object.fromEntries(formData) as T;
-    await handleSubmit(fieldValues);
+    const errors = await validate(fieldValues);
+    setErrors(errors);
+    await handleSubmit(fieldValues, errors);
     return fieldValues;
   }
 
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleBlur = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    if (validationSchema) {
+      const fieldSchema = validationSchema.pick([name]);
+      fieldSchema
+        .validate({ [name]: value })
+        .then(() => setErrors((currErrors) => ({ ...currErrors, [name]: '' })))
+        .catch((err) =>
+          setErrors((currErrors) => ({ ...currErrors, [name]: err.message }))
+        );
+    }
+    setTouched((prev) => ({ ...prev, [name]: true }));
+  };
+
+  const register = (name: keyof T) => ({
+    name,
+    onChange: handleChange,
+    onBlur: handleBlur,
+  });
+
   return {
-    formAction,
     pending,
     formState,
     submitCount,
+    touched,
+    errors,
+    formData,
+    formAction,
+    register,
   };
 }
 
